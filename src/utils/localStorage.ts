e988c10e631b2e56fbee77598ae61c0968702787
patatita <44934078+patatitta@users.noreply.github.com>
2026-03-13 13:58:51 +0100

    !Fix changed the very ugly method to display the cart into a non ugly method.

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,18 +1,49 @@
-      import Alpine from 'alpinejs'
-      import collapse from '@alpinejs/collapse'
-    Alpine.plugin(collapse)
-    Alpine.start()
+  function clonar() {
+    const menuTallas = document.querySelector("#menuProductos");
+    const stored = localStorage.getItem("carrito");
 
-function guardar(talla: any, producto: any){
-    let objeto = JSON.stringify({
-    talla: talla,
-    producto: producto
-})
-localStorage.setItem('carrito', objeto)
-}
 
-export{guardar};
+    console.log(stored)
+    console.log(menuTallas)
 
-function leer(){
-    return (Alpine.store('carrito'))
-}
+    let carritoProducto = document.querySelector(".carritoProducto");
+    if (stored == null || carritoProducto == null) {
+      return;
+    }
+
+    let arrayCarrito = JSON.parse(stored);
+    for (let iterable of arrayCarrito) {
+      let clon = carritoProducto.cloneNode(true) as Element;
+      if (clon == null || clon.querySelector(".nombre") == null) {
+        return;
+      }
+      const nombreElem = clon.querySelector(".nombre");
+      if (nombreElem == null) {
+        return;
+      }
+      nombreElem.textContent = iterable.nombre;
+      
+      const imagenElem = clon.querySelector(".imagen");
+      if (imagenElem == null) {
+        return;
+      }
+      imagenElem.src=iterable.imagen+"?w=125&format=auto";
+      
+
+  document.addEventListener('alpine:initialized', () => {
+      const datos = Alpine.$data(clon);
+      datos.cantidad = iterable.cantidad;
+        datos.precio = iterable.precio
+      datos.talla = iterable.talla
+      datos.color = iterable.color
+      datos.id = iterable.id
+  });
+
+  
+
+
+
+      menuTallas?.append(clon);
+    }
+  }
+  export default clonar;
